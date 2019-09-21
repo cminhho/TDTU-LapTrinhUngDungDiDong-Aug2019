d75ac353cb004faec6d6204dede96fe2eead9945
ChungHo <hmchung92@gmail.com>
2019-09-21 15:47:14 +0700

    Update manifest for all activities

--- Lab04Ca3/app/src/main/java/vn/edu/tdtu/elit/lab05/lab04/Exercise1.java
+++ Lab04Ca3/app/src/main/java/vn/edu/tdtu/elit/lab05/lab04/Exercise1.java
@@ -9,11 +9,12 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Exercise1 extends AppCompatActivity {
 
   private ListView lvUsers;
-  private ArrayList<String> items;
+  private List<String> items;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
